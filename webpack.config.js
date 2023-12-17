0063c71848f4603b9825daea2744c7e31fd57be1
Letiana0622 <letia@mail.ru>
2023-12-17 14:28:48 +0300

    legend version1

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const commonConfig = require('./webpack.common.config.js');
+const {merge} = require('webpack-merge');
+
+const productionConfig = require('./webpack.production.config.js');
+
+const developmentConfig = require('./webpack.development.config.js');
+
+module.exports = function(env) {
+  console.log(env);
+  if(env.development) {
+    return merge(commonConfig, developmentConfig);
+  }
+  else {
+    return merge(commonConfig, productionConfig);
+  };
+}
